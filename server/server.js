cafc963c77c9cced0b2abdc287d0e2f93580a065
Emanuel.Rusu <146270038+Rusu91-webdeveloper@users.noreply.github.com>
2024-10-27 13:52:59 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://nexus-management.onrender.com",
+      "https://nexus-management-frontend.onrender.com",
     ],
     credentials: true,
   })
