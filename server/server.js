a4b5c0e56060334042f232da1c34b7b8dd606f32
EmanuelDci2023 <Rusu.Emanuel.webdeveloper@gmail.com>
2024-10-27 13:16:33 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -11,54 +11,52 @@ import dashboard_router from "./Routes/Dashboard.Router.js";
 import room_router from "./Routes/Room.Router.js";
 import booking_router from "./Routes/Booking.Router.js.js";
 
-import fileUpload from 'express-fileupload'
-import {delete_pics, staff_pic_delete, staff_pic_upload, upload_router} from "./HelperFNs/Upload.js";
-import { v2 as cloudinary } from 'cloudinary';
+import fileUpload from "express-fileupload";
+import {
+  delete_pics,
+  staff_pic_delete,
+  staff_pic_upload,
+  upload_router,
+} from "./HelperFNs/Upload.js";
+import { v2 as cloudinary } from "cloudinary";
 import { email_sender } from "./HelperFNs/nodemailer.js";
 import review_router from "./Routes/Review.Router.js";
-
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cors(
-    {
-      origin: "http://localhost:5173",
-      credentials: true,
-    },
-    {
-      origin: "http://localhost:3000",
-      credentials: true,
-    }
-  )
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://nexus-management.onrender.com",
+    ],
+    credentials: true,
+  })
 );
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(fileUpload({useTempFiles:true}));
+app.use(fileUpload({ useTempFiles: true }));
 dotenv.config();
-
 
 cloudinary.config({
   cloud_name: process.env.cloud_name,
-  api_key:process.env.api_key,
-  api_secret:process.env.api_secret
-})
-
-
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 
 // Routers
 app.use("/dashboard", dashboard_router);
 app.use("/staff", staff_router);
 app.use("/booking", booking_router);
 app.use("/room", room_router);
-app.use("/room", upload_router)
-app.use("/room", delete_pics)
-app.use("/staff", staff_pic_upload)
-app.use("/staff/", staff_pic_delete)
-app.use("/review", review_router)
-
+app.use("/room", upload_router);
+app.use("/room", delete_pics);
+app.use("/staff", staff_pic_upload);
+app.use("/staff/", staff_pic_delete);
+app.use("/review", review_router);
 
 //Error Handler
 
